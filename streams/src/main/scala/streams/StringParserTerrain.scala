746200d39f7038ead9fa2d97b73287216d194b36
Sergey <myk1987@gmail.com>
2016-07-19 23:45:58 +0300

    Some changes

--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,8 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean =
+    (coord: Pos) => levelVector(coord.x)(coord.y) != '-'
 
   /**
    * This function should return the position of character `c` in the
