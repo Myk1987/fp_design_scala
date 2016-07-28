9bbeee0fddb035871741cc3fea81887d275e4c32
Sergey Moukavoztchik <myk1987@gmail.com>
2016-07-28 17:03:02 +0300

    Streams

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -1,7 +1,5 @@
 package streams
 
-import common._
-
 /**
  * This component implements the solver for the Bloxorz game
  */
@@ -10,7 +8,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.b1 == goal && b.b2 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +26,8 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] =
+    (for(n <- b.legalNeighbors) yield (n._1, n._2 :: history)).toStream
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +35,8 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] =
+    neighbors.filterNot(n => explored.contains(n._1))
 
   /**
    * The function `from` returns the stream of all possible paths
